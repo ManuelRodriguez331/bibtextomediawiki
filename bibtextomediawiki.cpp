8399bc4afead97266291085560409121765a12e1
ManuelRodriguez331 <32206350+ManuelRodriguez331@users.noreply.github.com>
2018-10-16 19:14:28 +0200

    Update bibtextomediawiki.cpp

--- bibtextomediawiki.cpp
+++ bibtextomediawiki.cpp
@@ -1,9 +1,16 @@
 /*
 g++ -std=c++14 bibtextomediawiki.cpp
 title: bibtex to Wikisyntax converter
-description: reads from file "input.txt" and stores result to out.txt
-date: 2018-10-13
+description: reads from file "input.bib" and stores result to "output.wiki"
+changelog:
+  2018-10-13 initial commit
+  2018-10-15 minor modication in filename handling
+  2018-10-15 shortformat
+  2018-10-16 <anchor> tag in fullmode
 to do: 
+  - context free grammar, bnf bibtext
+  - in Lineparser::run() subfunction which gets linenumber and lookuptablenumber 
+     -> new project from scratch too much changes
 */
 
 #include <iostream>
@@ -17,11 +24,13 @@ to do:
 class Textfile {
 public:
   std::vector<std::string> text; 
-  std::vector<std::string> output; 
+  std::vector<std::string> output;
+  std::string filenameinput="input.bib";
+  std::string filenameoutput="output.wiki"; 
   
-  void readfromfile(std::string filename) {
+  void readfromfile() {
     std::string line;
-    std::ifstream myfile (filename);
+    std::ifstream myfile (filenameinput);
     if (myfile.is_open()) {
       while ( getline (myfile,line) ) {
         text.push_back(line);
@@ -31,7 +40,7 @@ public:
   }
   void savetofile() {
     std::ofstream myfile;
-    myfile.open ("out.txt");
+    myfile.open (filenameoutput);
     for (auto i=0;i<output.size();i++)
       myfile << output[i];
     myfile.close();
@@ -49,11 +58,11 @@ class Lookuptable {
 public:
   std::vector<Item> mytable;
   Lookuptable() {
-    // organization is open
     mytable.push_back({"header","@article","{{cite journal"});
     mytable.push_back({"header","@inproceedings","{{cite conference"});
-    mytable.push_back({"header","@phdthesis","{{cite thesis\n| type = PhD"});
-    
+    mytable.push_back({"header","@phdthesis","{{cite thesis| type = PhD"});
+    mytable.push_back({"header","@techreport","{{cite techreport"});
+
     mytable.push_back({"field","booktitle","conference"});
     mytable.push_back({"field","title","title"});
     mytable.push_back({"field","author","author"});
@@ -61,15 +70,17 @@ public:
     mytable.push_back({"field","pages","pages"});
     mytable.push_back({"field","year","year"});
     mytable.push_back({"field","publisher","publisher"});
+    mytable.push_back({"field","organization","publisher"});
     mytable.push_back({"field","school","publisher"});
     mytable.push_back({"field","volume","volume"});
-
+    mytable.push_back({"field","institution","institution"});
   }
 };
   
 class Lineparser {
 public:
   Lookuptable mylookuptable;
+  bool fullformat;
   
   /// extract text between characters
   std::string stripline(std::string text, std::string leftsymbol, std::string rightsymbol) {
@@ -85,27 +96,40 @@ public:
   }
   std::string run(std::string line) {
     std::string result="??parseerror??";
+    result +=line;
     /// match lookuptable against line
     for (auto i=0;i<mylookuptable.mytable.size();i++) {
       if (findstring(line, mylookuptable.mytable[i].bibtex)) {
         if (mylookuptable.mytable[i].type=="header") {
-          result="<ref name=\"";
-          result+=stripline(line,"{",",");
-          result+="\"> ";
+          if (fullformat==true) {
+            // set anchor for reference
+            result=""; // clear variable
+            result+="<ref name=\"";
+            result+=stripline(line,"{",",");
+            result+="\" />\n";
+            // name for reference
+            result+="<ref name=\"";
+            result+=stripline(line,"{",",");
+            result+="\">";
+          }
+          else { // small reference tag
+            result=""; // clear variable
+            result+="<ref>";
+          }
           result+=mylookuptable.mytable[i].wikisyntax;
         }
         if (mylookuptable.mytable[i].type=="field") {
-          result="| ";
+          result=" |";
           result +=mylookuptable.mytable[i].wikisyntax;
-          result +=" = ";
+          result +="=";
           result +=stripline(line,"{","}");     
           break; // only search for first entry in table
         }
       }
-      if (line.find("}")== 0) // closing brackets on position 0
+      if (line.find("}")== 0) // closing brackets at position 0
         result="}}</ref>\n";
     }
-    result+="\n";
+    if (fullformat==true) result+="\n";
     return result;
   }
 };
@@ -115,8 +139,9 @@ public:
   Textfile mytextfile;
   Lineparser mylineparser;
   int verbose=0; //1==on
-  Converter() {
-    mytextfile.readfromfile("input.txt");
+
+  void formattextfile(bool fullformat) {
+    mylineparser.fullformat=fullformat;
     for (auto i=0;i<mytextfile.text.size();i++) { // go through text
       auto line=mytextfile.text[i];
       auto wikisyntax=mylineparser.run(line);
@@ -124,6 +149,11 @@ public:
         std::cout<<i<<" "<<line<<"\n";
       mytextfile.output.push_back(wikisyntax); // store to file
     }
+  }
+  Converter() {
+    mytextfile.readfromfile();
+    formattextfile(true);
+    formattextfile(false);
     mytextfile.savetofile();
   }
 };
